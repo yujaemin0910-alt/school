1b652ed7d8e16d29560eaaf1a0bf6a8f9870e355
yujaemin0910 <yujaemin0910@gmail.com>
2026-04-12 06:05:07 +0000

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-export CLOUDFLARE_API_TOKEN=cfut_aMkIodWQ5baFEN4ZGdQLRNmfeoSrzaPAxOA1PVxAecd7fa54
+export CLOUDFLARE_API_TOKEN=cfut_kGVMNMU12Fg1d7im1Rmo1PE8B0gveqji6dc9BTMZb0fc1559
 git add .
 git commit -m "update"
 git push origin main
